2f40d0382cb6532aa970ce3746abf757f7dafeb3
Code-Chetan <chetankinge@gmail.com>
2024-02-08 18:37:38 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello Chetan Welcome to Java");
-		System.out.println("This is test program for Jenkins");
+		System.out.println("This is test program for Jenkin");
 	}
 }
