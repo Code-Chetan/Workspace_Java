61864eed0238ce8c8fdef7d333fb75b025ffe6f7
Code-Chetan <chetankinge@gmail.com>
2024-02-09 13:41:50 +0530

    Student class added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ class HelloWorld{
 		System.out.println("Hello Chetan Welcome to Java");
 		System.out.println("This is test program for Jenkin");
 		System.out.println("Added 3rd line in the program");
+
+		Student student=new Student(14, "Sushant");
+		student.getStudentName();
 	}
 }
---
+++ Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    private int studentId;
+    private String studentName;
+
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+
+    public int getStudentId() {
+        return this.studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return this.studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+}
